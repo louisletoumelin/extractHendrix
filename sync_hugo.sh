253c07b34e1ec0eb79f8689439b287b657fcf2f2
MERZISEN Hugo <hugo.merzisen@meteo.fr>
2022-03-25 15:17:00 +0100

    removed unneeded rsync script

--- sync_hugo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rsync \
-	-av \
-	--exclude /.git/ \
-	--delete \
-	/home/hugo/Projets/01_plomberie/extracthendrix/ merzisenh@sxcen.cnrm.meteo.fr:/home/merzisenh/RSYNCED_CODES/extracthendrix/
